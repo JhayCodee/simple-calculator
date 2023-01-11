fa5de77e24c78c4acb7d56154ba8040fac6b3d8e
Luis Vargas <kluis0178@gmail.com>
2023-01-11 01:02:51 -0600

    basic html and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import logo from "./img/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App">hola</div>;
+  return (
+    <div className="App">
+      <div className="app-logo-container">
+        <img src={logo} className="app-logo" alt="app-logo" />
+      </div>
+
+      <div className="calculator-container">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
